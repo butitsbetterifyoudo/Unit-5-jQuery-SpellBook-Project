cff3b2711aba75d757b49fe58370e01e7fcea4f9
butitsbetterifyoudo <43342192+butitsbetterifyoudo@users.noreply.github.com>
2019-01-06 17:00:53 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,25 @@
+//Spell #1
+
+$(".cards").click(function(){
+    $(".cards").fadeOut();
+});
+
+
+
+//Spell #2
+
+$(".grade").click(function(){
+ $(".grade").slideToggle();
+});
+
+
+
+//Spell #3
+
+$(".soul").click(function(){
+    $(".soul").toggle();
+ 
+});
+
+
+
